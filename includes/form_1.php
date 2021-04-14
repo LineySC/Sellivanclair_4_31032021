bbf764b2ed6b0a27509e44f7a55eff922bba0953
Sellivan <tahwiza331@gmail.com>
2021-04-14 12:37:08 +0200

    Traduction en français

--- includes/form_1.php
+++ includes/form_1.php
@@ -12,9 +12,9 @@
 	$to = 'receiver@yoursite.com'; // Email submissions are sent to this email
 
 	// Create email	
-	$email_subject = "Message from your website";
-	$email_body = "You have received a new message. \n\n".
-				  "Name: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
+	$email_subject = "Message de votre site";
+	$email_body = "Vous avez reçu un nouveau message. \n\n".
+				  "Nom: $name \nEmail: $email \nInput_504: $input_504 \nMessage: $message \n";
 	$headers = "MIME-Version: 1.0\r\nContent-type: text/plain; charset=UTF-8\r\n";	
 	$headers .= "From: contact@yoursite.com\n";
 	$headers .= "Reply-To: $input_504";	
